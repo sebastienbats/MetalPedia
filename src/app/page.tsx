4206dc69d51c68d088646ece3f6ab20dca6f3439
BATS SÉBASTIEN <sebastien.bats@gmail.com>
2026-09-24 21:03:59 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,7 @@ export default async function HomePage() {
         />
       </div>
 
-      <div className="relative z-20 container mx-auto px-4 py-12 lg:py-16">
+      <div className="relative z-20 container mx-auto px-4 pt-4 pb-12 lg:pb-16">
         <div className="text-center mb-8">
           <h1 className="font-metal text-2xl lg:text-4xl text-metal-fire mb-8 drop-shadow-lg">
             Bienvenue dans le Metalverse
